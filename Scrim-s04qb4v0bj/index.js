c2d9f114c1eb2ae492d166ddf84023338f1937fe
runnock <102752473+runnock@users.noreply.github.com>
2024-10-04 21:36:10 -0600

    Add files via upload

---
+++ Scrim-s04qb4v0bj/index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}
+
+function erase() {
+    countEl.textContent = 0
+    countReset(count)
+}
+
+
+
